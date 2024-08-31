2845b739f7c83f5045dc1236a87b6926e1021fb5
Fahad Alam <fahadalam12405@gmail.com>
2024-08-31 19:22:07 +0500

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -95,7 +95,7 @@ function NavBar() {
             </Nav.Item>
             <Nav.Item>
               <Nav.Link
-                href="https://zenith-e-folio.tiiny.site"
+                href="https://fahadalam.tiiny.site"
                 target="_blank"
                 rel="noreferrer"
               >
